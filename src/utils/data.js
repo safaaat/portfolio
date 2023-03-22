7e88e80c1e69318e663c4d859d06af0db0d9a81c
Muhammad Safaat <paatlupi01@gmail.com>
2023-03-22 22:34:47 +0700

    update Data

--- src/utils/data.js
+++ src/utils/data.js
@@ -11,7 +11,7 @@ const dataPortfolio = [
     {
         id: 2,
         judul: "Catatan",
-        keterangan: "Catatan adalah website untuk mengimpan catatan yang hanya berupa text. fitur yang tersedia berupa login, register, tambah catatan, tambah folder, remove checkbox catatan dan folder, untuk bagian FontEnd menggunakan React Js, sedangkan bagian BackEnd menggunakan Node Js",
+        keterangan: "Catatan adalah website untuk menyimpan catatan yang hanya berupa text. fitur yang tersedia berupa login, register, tambah catatan, tambah folder, remove checkbox catatan dan folder. Untuk bagian FontEnd menggunakan React Js, sedangkan bagian BackEnd menggunakan Node Js",
         teknologi: ["React Js", "|", "Taillwind Css", "|", "Redux", "|", "Node Js", "|", "Sequelize", "|", "express", "|", "argon2", "|", "SQL"],
         linkGithub: "https://github.com/safaaat/fontend_catatan",
         linkWebsite: "https://safaaat.github.io/fontend_catatan",
